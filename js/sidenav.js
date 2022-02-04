aea69a9779703976caa57347302807a0c09b82c2
Gauravtripathii <gauravtripathii7979@gmail.com>
2022-02-04 13:56:44 +0530

    sidenav commit

---
+++ js/sidenav.js
@@ -0,0 +1,9 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.querySelector(".trans-cover").style.display = "block";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.querySelector(".trans-cover").style.display = "none";
+}
